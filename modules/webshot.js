02bd169b40148acd93977388dbaef9b2d7b5fc01
Michael Cao <mike.x.cao@gmail.com>
2017-07-16 22:13:37 -0400

    7/16/2017

---
+++ modules/webshot.js
@@ -0,0 +1,23 @@
+const printscreen = require('printscreen');
+
+module.exports = {
+    name: 'webshot',
+    type: 'utility',
+    usage: 'webshot <website>',
+    permission: 1,
+    help: 'Returns a screenshot of a website.',
+    main: function (bot, msg) {
+        if(msg.content.indexOf("http") < 0) return "Please specify a website with http(s) included!";
+        printscreen(msg.content, {
+            viewport: {
+                width: 1920,
+                height: 1080
+            },
+            timeout: 1000,
+            format: 'png',
+            quality: 75,
+        }, (err, data) => {
+            msg.channel.send("Screenshot of <" + msg.content + ">", {file:data.file})
+        });
+    }
+};
