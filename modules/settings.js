a5c2c1787e8b076fcc6569b08d85f3b81eefcdfb
Michael Cao <mike.x.cao@gmail.com>
2017-07-30 21:56:02 -0400

    Settings Fixes

--- modules/settings.js
+++ modules/settings.js
@@ -9,11 +9,11 @@ module.exports = {
 	usage: 'settings <arguments>',
 	permission: 4,
 	help: 'Changes server settings.',
-	main: function (bot, msg) {
+	main: function(bot, msg) {
 		if (!msg.member.hasPermission('MANAGE_GUILD')) return msg.reply("you do not have permission to manage this server's setings!")
 
 		if (msg.args[0] == "announcementchannel") {
-
+			
 		} else if (msg.args[0] == "welcomemessage") {
 			bot.getWelcomeMessageStatus(msg.guild.id).then(value => {
 				welcomeMessageEnabled(value);
@@ -31,19 +31,19 @@ module.exports = {
 		} else if (msg.args[0] == "mentionspam") {
 
 		}
-
+		
 		msg.reply("please specify an argument! Accepted arguments: announcementchannel, welcomemessage, leavemessage, banmessage, joinrole, botrole, invitelinks, mentionspam")
 
 		function welcomeMessageEnabled(value) {
 			msg.channel.send(`The welcome message for this server is **${value ? 'on' : 'off'}**. Do you want to turn it **${value ? 'off' : 'on'}**? (Reply with 'yes' or 'no')`);
-			var collector = msg.channel.createCollector(
-				m => (m.content.toLowerCase() == 'yes' || m.content.toLowerCase() == 'no'),
+			var collector = msg.channel.createCollector (
+				m => (m.content.toLowerCase()== 'yes' || m.content.toLowerCase() == 'no'),
 				{ time: 15000 }
 			);
 			collector.on('message', m => {
 				var e = value;
-				if (m.content.toLowerCase() == 'yes' && m.author.id == msg.author.id) {
-					if (value)
+				if(m.content.toLowerCase() == 'yes' && m.author.id == msg.author.id) {
+				if(value)
 						value = 0
 					else
 						value = 1
@@ -51,24 +51,27 @@ module.exports = {
 					msg.channel.send(`Welcome messages ${e ? 'enabled' : 'disabled'}.`);
 					collector.stop();
 				}
-				if (e) {
-					msg.channel.send("What would you like the welcome message to be? You may include the following arguments in your welcome message: ``{servername}``, ``{username}``, ``{usermention}``, ``{userdiscrim}``, ``{membercount}``")
-					var collector = msg.channel.createCollector(
-						m => msg.author.id == m.author.id,
-						{ time: 15000 }
-					);
-					collector.on('message', m => {
-						msg.channel.send(`Welcome message set to \`${bot.setWelcomeMessageText(m.content)}\`!`);
-					});
-					collector.on('end', collected => {
-						if (collected.size == 0)
-							msg.channel.send("No messages were detected within 15 seconds. Aborting...")
-						console.log(`Collected ${collected.size} items`)
-					});
-				}
+				if(e)
+					getNewWelcomeMessage();
 			});
 			collector.on('end', collected => {
-				if (collected.size == 0)
+				if(collected.size == 0)
+					msg.channel.send("No messages were detected within 15 seconds. Aborting...")
+				console.log(`Collected ${collected.size} items`)
+			});
+		}
+
+		function getNewWelcomeMessage() {
+			msg.channel.send("What would you like the welcome message to be? You may include the following arguments in your welcome message: ``{servername}``, ``{username}``, ``{usermention}``, ``{userdiscrim}``, ``{membercount}``")
+			var collector = msg.channel.createCollector (
+				m => msg.author.id == m.author.id,
+				{ time: 15000 }
+			);
+			collector.on('message', m => {
+				msg.channel.send(`Welcome message set to \`${bot.setWelcomeMessageText(m.content)}\`!`);
+			});
+			collector.on('end', collected => {
+				if(collected.size == 0)
 					msg.channel.send("No messages were detected within 15 seconds. Aborting...")
 				console.log(`Collected ${collected.size} items`)
 			});
