096ec56935f1f256f95a9f144613043b3eb4ab58
Michael Cao <mike.x.cao@gmail.com>
2017-07-30 23:43:13 -0400

    oops

--- modules/blacklist.js
+++ modules/blacklist.js
@@ -6,17 +6,17 @@ module.exports = {
 	name: 'blacklist',
     type: 'core',
 	usage: 'blacklist <id>',
-	permission: 1,
+	permission: 6,
 	help: 'Adds a user to the blacklist.',
 	main: function(bot, msg) {
         let id = msg.content;
         for(var i = 0; i < blacklist.length; i++) {
             if(blacklist[i] == id)
-                msg.reply("they're already blacklisted you idiot!")
+                return msg.reply("they're already blacklisted you idiot!")
             else {
                 blacklist.push(id);
                 fs.writeFileSync("./blacklist.json", JSON.stringify(blacklist, null, 3));
-                msg.channel.send("User ID " + msg.content + " has been blacklisted!");
+                return msg.channel.send("User ID " + msg.content + " has been blacklisted!");
             }
         }
 	}
