093de61ac5d9f2ffe27a3af49adf89052c7a2cf2
Michael Cao <mike.x.cao@gmail.com>
2017-07-30 23:46:41 -0400

    did I get it right this time?

--- modules/blacklist.js
+++ modules/blacklist.js
@@ -9,15 +9,20 @@ module.exports = {
 	permission: 6,
 	help: 'Adds a user to the blacklist.',
 	main: function(bot, msg) {
-        let id = msg.content;
-        for(var i = 0; i < blacklist.length; i++) {
-            if(blacklist[i] == id)
-                return msg.reply("they're already blacklisted you idiot!")
-            else {
-                blacklist.push(id);
-                fs.writeFileSync("./blacklist.json", JSON.stringify(blacklist, null, 3));
-                return msg.channel.send("User ID " + msg.content + " has been blacklisted!");
+        let id = msg.content,
+            found = false,
+            i = 0;
+        while(i < blacklist.length && !found) {
+            if(blacklist[i] == id) {
+                found = true;
+                msg.reply("they're already blacklisted you idiot!")
             }
+            i++;
+        }
+        if(!found) {
+            blacklist.push(id);
+            fs.writeFileSync("./blacklist.json", JSON.stringify(blacklist, null, 3));
+            msg.channel.send("User ID " + msg.content + " has been blacklisted!");
         }
 	}
 };
