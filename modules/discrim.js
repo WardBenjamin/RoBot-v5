fe24b43b14a3e96baafea92dddeadc168ab36e52
Michael Cao <mike.x.cao@gmail.com>
2017-07-31 14:44:17 -0400

    improved discrim

--- modules/discrim.js
+++ modules/discrim.js
@@ -5,16 +5,23 @@ module.exports = {
     permission: 1,
     help: 'Searches for a discriminator.',
     main: function (bot, msg) {
-        var discrim = msg.content || msg.author.discriminator,
+        var discrim = msg.content,
             usernames = "";
-        msg.channel.send("Hunting for other users with the discriminator #" + discrim + "...")
-        bot.users.forEach(user => {
-            if(user.discriminator == discrim)
-                usernames += user.username + "\n";
-        });
-        if(usernames == "")
-            msg.channel.send("There are no other users with your discriminator in my cache :cry:")
-        else
-            msg.channel.send("Change your username to one of the following to change your discriminator:```" + usernames + "```")
+        if (discrim == null)
+            discrim = msg.author.discriminator;
+
+        if (discrum.length != 4)
+            msg.channel.send("That is not a valid discriminator!")
+        else {
+            msg.channel.send("Hunting for other users with the discriminator #" + discrim + "...")
+            bot.users.forEach(user => {
+                if (user.discriminator == discrim)
+                    usernames += user.username + "\n";
+            });
+            if (usernames == "")
+                msg.channel.send("There are no other users with your discriminator in my cache :cry:")
+            else
+                msg.channel.send("Change your username to one of the following to change your discriminator:```" + usernames + "```")
+        }
     }
 };
