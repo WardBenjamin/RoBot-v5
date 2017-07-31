0b3f8e92ae795bde0e1cf40b6fb29cc624294691
Michael Cao <mike.x.cao@gmail.com>
2017-07-30 23:40:57 -0400

    More blacklist cmds

--- modules/blacklist.js
+++ modules/blacklist.js
@@ -9,9 +9,15 @@ module.exports = {
 	permission: 1,
 	help: 'Adds a user to the blacklist.',
 	main: function(bot, msg) {
-		let id = msg.content;
-        blacklist.push(id);
-        fs.writeFileSync("./blacklist.json", JSON.stringify(blacklist, null, 3));
-        msg.channel.send("User ID " + msg.content + " has been blacklisted!");
+        let id = msg.content;
+        for(var i = 0; i < blacklist.length; i++) {
+            if(blacklist[i] == id)
+                msg.reply("they're already blacklisted you idiot!")
+            else {
+                blacklist.push(id);
+                fs.writeFileSync("./blacklist.json", JSON.stringify(blacklist, null, 3));
+                msg.channel.send("User ID " + msg.content + " has been blacklisted!");
+            }
+        }
 	}
 };
